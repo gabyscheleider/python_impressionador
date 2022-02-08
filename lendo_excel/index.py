0933936c31603399a4e391791ccf3e1118902e10
desenvolvimentodaju <gayscheleider@gmail.com>
2022-02-08 12:26:43 -0300

    Lendo csv excel

--- lendo_excel/index.py
+++ lendo_excel/index.py
@@ -1,5 +1,23 @@
 import pandas as pd
+import mysql.connector
+from mysql.connector import Error
 
-tabela = pd.read_csv('realizadoMargem.csv')
+margem_df = pd.read_csv('realizadoMargem.csv', sep=';')
 
-print(tabela)
+try:
+    conn = mysql.connector.connect(host='portaldaju.com.br',database='wwport_db_daju',user='wwport_administrator',password='Daju.620@@')
+    for i in range(margem_df.shape[0]):
+        data = pd.to_datetime(margem_df['dt.transacao'][i], format='%d/%m/%Y').date()
+        insert_info = "INSERT INTO `margem_produtos`(`id`, `nr_transacao`, `dt_transacao`, `cd_produto`, `ds_artigo`, `cst`, `cmv`, `cm_porcent`, `vlr_cm`) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(0, margem_df['nr.transacao'][i],data,margem_df['Cod. Produto'][i],margem_df['Ds. Artigo'][i],margem_df['cst'][i], margem_df['CMV+CRIT_CALC.'][i],margem_df['%_CM'][i],margem_df['VLR._CM'][i])
+        print(insert_info)
+        cursor = conn.cursor()
+        cursor.execute(insert_info)
+        conn.commit()
+        cursor.close()
+except Error as erro:
+    print("Falha ao inserir dados: {}".format(erro))
+finally:
+    if(conn.is_connected()):
+        cursor.close()
+        conn.close()
+        print('Conexão ao MySql Finalizada forçadamente!')
